6795398e7fec4c77c66b03fc007444c8a696d6d4
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-04-10 22:43:07 +0100

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Logo from "/logo.jpg";
+
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={Logo} alt="Logo" />
+        <h1>React food</h1>
+      </div>
+    </header>
+  );
+}
