c6a20d5624f18502950b6053e5e0aed2f5e9808c
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-04-11 20:22:06 +0100

    Add itemsLength to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
+import { useContext } from "react";
 import Button from "./UI/Button";
 import Logo from "/logo.jpg";
+import { CartContext } from "../store/CartContextProvider";
 
 export default function Header() {
+  const cartContext = useContext(CartContext);
+
+  const itemsLength = cartContext.cart.items.reduce(
+    (acc, curr) => acc + curr.quantity,
+    0,
+  );
+
   return (
     <header id="main-header">
       <div id="title">
         <img src={Logo} alt="Logo" />
         <h1>React food</h1>
       </div>
-      <Button textOnly>Cart</Button>
+      <Button textOnly>Cart ({itemsLength})</Button>
     </header>
   );
 }
