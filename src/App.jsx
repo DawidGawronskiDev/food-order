4a33a6ac493f79cf0041f48ac42de59398000c72
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-04-11 19:07:18 +0100

    Refactor Meals component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
-import useFetch from "./hooks/useFetch";
 import CartContextProvider from "./store/CartContextProvider";
 
 function App() {
-  const { data, error, isLoading } = useFetch("http://localhost:3000/meals");
-
-  if (isLoading) {
-    return <span>Loading...</span>;
-  }
-
   return (
     <CartContextProvider>
       <Header />
-      {isLoading ? (
-        <span>Loading...</span>
-      ) : (
-        <main>
-          <Meals meals={data} />
-        </main>
-      )}
+      <Meals />
     </CartContextProvider>
   );
 }
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,6 +1,21 @@
+import useFetch from "../hooks/useFetch";
 import MealItem from "./MealItem";
 
-export default function Meals({ meals }) {
+export default function Meals() {
+  const {
+    data: meals,
+    error,
+    isLoading,
+  } = useFetch("http://localhost:3000/meals");
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (error) {
+    return <span>{error}</span>;
+  }
+
   return (
     <ul id="meals">
       {meals && meals.map((meal) => <MealItem key={meal.id} meal={meal} />)}
