0af9d37d023f2b9ea32ac12f2286e10335de035e
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-04-12 11:14:27 +0100

    Create handleCloseCart in Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -16,18 +16,26 @@ export default function Cart() {
     0,
   );
 
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
+
   return (
     <Modal open={userProgressCtx.progress === "cart"} className="cart">
       <h2>My Cart</h2>
       <ul>
         {cartCtx.cart.items.map((item) => (
-          <li key={item.id}>{item.id}</li>
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
         ))}
       </ul>
       <p className="cart-total">$ {cartTotal}</p>
       <p className="cart-actions">
-        <Button textOnly>Close</Button>
-        <Button>Go to checkout</Button>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        <Button onClick={handleCloseCart}>Go to checkout</Button>
       </p>
     </Modal>
   );
